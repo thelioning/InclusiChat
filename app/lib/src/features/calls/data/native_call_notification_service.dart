c7d6f4700ba28c33aa4d7e184b2e8bc8ea5ae712
Ermogenes Rodríguez Fernandez <86388515+thelioning@users.noreply.github.com>
2026-08-24 19:22:15 -0400

    chore(rep-002): remove unused foundation import

--- app/lib/src/features/calls/data/native_call_notification_service.dart
+++ app/lib/src/features/calls/data/native_call_notification_service.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_callkit_incoming/entities/android_params.dart';
 import 'package:flutter_callkit_incoming/entities/call_event.dart';
