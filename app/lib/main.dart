74bc92507d59c2679b636efc1c639cdb1412055a
Ermogenes Rodríguez Fernandez <86388515+thelioning@users.noreply.github.com>
2026-08-31 14:29:07 -0400

    feat: register Firebase background message handler

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
+import 'src/core/message_push_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
 
   const url = String.fromEnvironment('SUPABASE_URL');
   const publishableKey = String.fromEnvironment('SUPABASE_PUBLISHABLE_KEY');
